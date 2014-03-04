4577d3020c1337916e26b383d43d6415a8613926
Michael Häfner <haefnermichael@gmx.de>
2014-03-04 14:56:22 +0100

    new width correction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Code goes here
 var app = angular.module('myApp', []);
 
-app.directive('mhVerticalScroll', function() {
+app.directive('mhVerticalScroll', [ '$window', function(  $window ) {
   return {
     restrict: 'A',
     compile: function(el,attrs) {
@@ -13,75 +13,84 @@ app.directive('mhVerticalScroll', function() {
       var width = parent.width();
       
       // calculate the real width
-      el.css({"position": "absolute"});
-      var realWidth = el.width();
-      
-      parent.css({
-        "position": "relative",
-        "overflow": "hidden"
+      var realWidth = 0;
+      el.children().each(function(i) {
+        realWidth += $(this).width();
       });
       
-      el.css({
-        "position": "relative",
-        "display": "block",
-        "width": realWidth
-      });
-      
-      var scrollerWidth = width*width/realWidth;
-      scroller.css({"width": scrollerWidth});
-      
-      scrollbar.append(scroller);
-      parent.append(scrollbar);
-      
+      // add a scrollbar only if the content does not fit
+      if (realWidth > width) {
+        parent.css({
+          "position": "relative",
+          "overflow": "hidden"
+        });
+        
+        el.css({
+          "position": "relative",
+          "display": "block",
+          "width": realWidth
+        });
+
+        scrollbar.css({
+          "width": width
+        });
+        
+        var scrollerWidth = width*width/realWidth;
+        scroller.css({"width": scrollerWidth});
+        
+        scrollbar.append(scroller);
+        parent.append(scrollbar);
+      }
       return function(scope,elem,attrs) {
-        //linking function here
-        
-        $.data(parent, "verticalScroll", false);
-        
-        //desktop scrolling
-        parent.mousedown(function(ev) {
-          $.data(parent, "verticalScroll", true);
-          $.data(parent, "startX", ev.pageX );
-        });
-        
-        parent.mousemove(function(ev) {
-          if ( $.data(parent, "verticalScroll") ) {
-            
-            var x = ev.pageX - $.data(parent, "startX");
+        //only do something if the content does not fit
+        if (realWidth > width) {
+          $.data(parent, "verticalScroll", false);
+          
+          //desktop scrolling
+          parent.mousedown(function(ev) {
+            $.data(parent, "verticalScroll", true);
             $.data(parent, "startX", ev.pageX );
-            
-            slideVertical(x);
-          }
-        });
-        
-        parent.mouseup(function() {
-          $.data(parent, "verticalScroll", false);
-        });
-        
-        parent.mouseout(function() {
-          $.data(parent, "verticalScroll", false);
-        });
-        
-        
-        //touch scrolling
-        parent.bind("touchstart", function(ev) {
-          $.data(parent, "verticalScroll", true);
-          $.data(parent, "startX", ev.pageX );
-        });
-        
-        parent.bind("touchmove", function(ev) {
-          if ( $.data(parent, "verticalScroll") ) {
-            
-            var x = ev.pageX - $.data(parent, "startX");
+          });
+          
+          parent.mousemove(function(ev) {
+            if ( $.data(parent, "verticalScroll") ) {
+              
+              var x = ev.pageX - $.data(parent, "startX");
+              $.data(parent, "startX", ev.pageX );
+              
+              slideVertical(x);
+            }
+          });
+          
+          parent.mouseup(function() {
+            $.data(parent, "verticalScroll", false);
+          });
+          
+          parent.mouseout(function() {
+            $.data(parent, "verticalScroll", false);
+          });
+          
+          
+          //touch scrolling
+          parent.bind("touchstart", function(ev) {
+            $.data(parent, "verticalScroll", true);
             $.data(parent, "startX", ev.pageX );
-            
-            slideVertical(x);
-          }
-        });
-        
-        parent.bind("touchend", function(ev) {
-          $.data(parent, "verticalScroll", false);
-        });
+          });
+          
+          parent.bind("touchmove", function(ev) {
+            if ( $.data(parent, "verticalScroll") ) {
+              
+              var x = ev.pageX - $.data(parent, "startX");
+              $.data(parent, "startX", ev.pageX );
+              
+              slideVertical(x);
+            }
+          });
+          
+          parent.bind("touchend", function(ev) {
+            $.data(parent, "verticalScroll", false);
+          });
+        }
         
         //slide the content vertically
         function slideVertical(x) {
@@ -103,4 +112,4 @@ app.directive('mhVerticalScroll', function() {
       };
     }
   }
-})
+}]);
